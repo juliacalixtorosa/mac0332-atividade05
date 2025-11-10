7de35502cfc132ec046fadf798366e6ff9a63ac5
Júlia Calixto Rosa <161134491+juliacalixtorosa@users.noreply.github.com>
2025-11-10 19:45:06 -0300

    Initial commit
    Signed-off-by: Júlia Calixto <juliacalixtorosaa@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+# Teste Estrutural (Caixa Branca)
+
+## Objetivo Principal
+
+Aplicar os conceitos de teste estrutural (caixa branca) para compreender o comportamento interno de um programa e avaliar a cobertura de código por meio da análise de nós, arestas e caminhos.
+
+O foco é permitir que os estudantes desenvolvam a capacidade de visualizar o fluxo lógico, identificar caminhos executáveis e não executáveis e projetar casos de teste eficazes que garantam a verificação completa da lógica interna.
+
+## Competências Desenvolvidas
+
+- Analisar a estrutura interna de um programa.
+- Construir o grafo de fluxo de controle (GFC).
+- Identificar nós, arestas e caminhos executáveis.
+- Aplicar critérios de cobertura estrutural (Todos-Nós, Todas-Arestas e Todos-Caminhos).
+- Projetar casos de teste mínimos que satisfaçam cada critério.
+- Utilizar ferramentas para visualizar e medir a cobertura de código.
---
+++ main.py
@@ -0,0 +1,19 @@
+def calcular_desconto(valor_compra, tipo_cliente, cupom):
+    if valor_compra <= 0:                                #1
+        return 0                                         #2
+
+    if tipo_cliente == "premium":                        #3          
+        desconto = 0.15                                  #4
+    elif tipo_cliente == "regular":                      #5
+        desconto = 0.05                                  #6
+    else:                                                #7
+        desconto = 0                                     #7                                     
+    if cupom == "DESC10" and valor_compra > 100:         #8
+        desconto += 0.10                                 #9
+
+    valor_final = valor_compra * (1 - desconto)          #10
+
+    if valor_final < 50:                                 #10
+        valor_final += 5  # taxa mínima de entrega       #11
+
+    return round(valor_final, 2)                         #12
